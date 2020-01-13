bcfe1391b8931c8af07d959ef5cdab2b3859124c
Aaron Hooper <rnjckhpr@gmail.com>
2020-01-13 18:30:18 +0000

    Add dummy values and render in view

--- routes/bookmarks.js
+++ routes/bookmarks.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+const bookmarks = [
+    {
+        title: "test bookmark 1",
+        url: "https://duckduckgo.com",
+        description: "A private search engine"
+    },
+    {
+        title: "test bookmark 2",
+        url: "https://duckduckgo.com",
+        description: "A private search engine"
+    },
+    {
+        title: "test bookmark 3",
+        url: "https://duckduckgo.com",
+        description: "A private search engine"
+    },
+];
+
 /* GET bookmarks listing. */
 router.get('/', function(req, res, next) {
-  res.render('bookmarks/index');
+  res.render('bookmarks/index', { bookmarks });
 });
 
 module.exports = router;
