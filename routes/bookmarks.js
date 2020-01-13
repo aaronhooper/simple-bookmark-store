d82e48d4a66ead68c439da450229e5d73d13aa51
Aaron Hooper <rnjckhpr@gmail.com>
2020-01-13 19:02:08 +0000

    Add edit bookmark route and view

--- routes/bookmarks.js
+++ routes/bookmarks.js
@@ -3,16 +3,19 @@ var router = express.Router();
 
 const bookmarks = [
     {
+        id: 1,
         title: "test bookmark 1",
         url: "https://duckduckgo.com",
         description: "A private search engine"
     },
     {
+        id: 2,
         title: "test bookmark 2",
         url: "https://duckduckgo.com",
         description: "A private search engine"
     },
     {
+        id: 3,
         title: "test bookmark 3",
         url: "https://duckduckgo.com",
         description: "A private search engine"
@@ -22,6 +25,11 @@ const bookmarks = [
 /* GET bookmarks listing. */
 router.get('/', function(req, res, next) {
   res.render('bookmarks/index', { bookmarks });
+});
+
+router.get('/:id', function(req, res, next) {
+    const bookmark = bookmarks.filter(bookmark => bookmark.id == req.params.id)[0];
+    res.render('bookmarks/bookmark', { bookmark });
 });
 
 module.exports = router;
