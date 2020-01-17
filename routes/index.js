d6039d3d6ce5f85316f73129a71dcfe043d6d5f1
Aaron Hooper <rnjckhpr@gmail.com>
2020-01-17 18:33:17 +0000

    Add register route

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 var usersRouter = require('./users');
 var bookmarksRouter = require('./bookmarks');
+var registerRouter = require('./register');
 
 router.use('/users', usersRouter);
 router.use('/bookmarks', bookmarksRouter);
+router.use('/register', registerRouter);
 
 router.get('/', function(req, res, next) {
     res.redirect('/bookmarks');
