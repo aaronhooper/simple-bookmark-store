d64fd09181b19400bf49fa099834c7db03ebe037
Aaron Hooper <rnjckhpr@gmail.com>
2020-01-17 17:38:56 +0000

    Add user model

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+module.exports = mongoose.model('User', userSchema);
