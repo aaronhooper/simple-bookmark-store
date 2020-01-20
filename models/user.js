3181e78ed068d1c8d0a0c580d510febc6b2af099
Aaron Hooper <rnjckhpr@gmail.com>
2020-01-20 17:23:45 +0000

    Add password compare method to user model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
     date: {
@@ -20,6 +21,10 @@ const userSchema = mongoose.Schema({
         required: true,
     },
 });
+
+userSchema.methods.validPassword = function (password) {
+    return bcrypt.compare(password, this.password);
+}
 
 // throws ValidationError when unique constraint is violated
 userSchema.plugin(uniqueValidator);
