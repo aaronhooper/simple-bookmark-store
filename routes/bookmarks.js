9e4d82fa9211d9c6122e7b814bea46d5381754af
Aaron Hooper <rnjckhpr@gmail.com>
2020-02-13 18:40:19 +0000

    Protect bookmarks route

--- routes/bookmarks.js
+++ routes/bookmarks.js
@@ -1,7 +1,16 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
 
 const bookmarks = require('../controllers/bookmarks');
+
+// TODO: move this to a more sensible place
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) { return next(); }
+    res.redirect('/login')
+}
+
+router.all('/', ensureAuthenticated);
 
 router.post('/', bookmarks.create, function (req, res) {
     res.redirect('/bookmarks');
